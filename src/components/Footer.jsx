9a6d64566402194174edd3491c9c069d056a0f8e
yy0213148 <43162443+yy0213148@users.noreply.github.com>
2025-09-27 13:10:04 +0530

    Footer Name Change and Remove the vite icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="py-6 text-center bg-black/30 backdrop-blur-md">
-      <p>© 2025 John Doe. Built with ❤️ using React, Tailwind & Framer Motion</p>
+      <p>© 2025 Yogesh R Yadav. Built with ❤️ using React, Tailwind & Framer Motion</p>
     </footer>
   )
 }
